e92effa1827ef1e69fc30212a50ccea560b8fad4
OkadaGenya <g_okada@waku-2.com>
2019-02-21 12:34:54 +0900

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# approval-counter
+
+## approveの数をカウントして表示するchrome拡張
+
+## Get started
+
+### ストアからインストール
+
+todo
+
+### manualy install
+
+1. git clone
+
+```
+git clone git@github.com:wilf312/approval-counter.git
+
+or
+
+git clone https://github.com/wilf312/approval-counter.git
+```
+
+2. chromeで拡張機能画面を開く
+
+メニューバー → ウィンドウ → 拡張機能
+
+3. 拡張を追加
+
+パッケージ化されていない拡張機能を追加 ボタンを押す → clone したリポジトリ フォルダを選択
---
+++ content.js
@@ -0,0 +1,35 @@
+
+// 要素生成
+var getStyledDom = (text) => {
+  var dom = document.createElement("div")
+  dom.textContent = text
+  dom.attributeStyleMap.set('position', 'absolute')
+  dom.attributeStyleMap.set('left', '45px')
+  dom.attributeStyleMap.set('top', '30px')
+  dom.attributeStyleMap.set('color', 'black')
+  dom.attributeStyleMap.set('font-size', '20px')
+  dom.classList.add('approval-counter')
+
+  return dom
+}
+
+// github が pjaxを使っているため、 ページングで拡張が再コールされない。
+// なので、 setInterval + querySelector('.approval-counter')存在チェックをしている
+setInterval(() => {
+  // review approval が含まれる要素を取得
+  var selectedDom = document.querySelectorAll('[aria-label*="review approval"]');
+  var approvalList = Array.from(selectedDom,  dom => dom)
+
+  approvalList.forEach(d => {
+    const parentNode = d.parentElement.parentElement.parentElement
+
+    if (parentNode.querySelector('.approval-counter')) {
+      return
+    }
+
+    const label = d.getAttribute("aria-label")
+    const approveCount = parseInt(label, 10)
+    const dom = getStyledDom(approveCount)
+    parentNode.appendChild(dom)
+  })
+}, 500)
