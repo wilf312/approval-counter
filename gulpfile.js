4d1b668f8fa78abce62f5df32980778f19f5aacd
OkadaGenya <g_okada@waku-2.com>
2019-02-21 13:15:55 +0900

    fix dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,11 @@ const gulp = require('gulp')
 const zip = require('gulp-zip')
 const del = require('del')
 
-const config = {
-  dist: './dist'
-}
-
-gulp.task('copy', () => {
-  gulp.src('./manifest.json').pipe(gulp.dest(config.dist))
-  return gulp.src('./content.js').pipe(gulp.dest(config.dist))
-})
-
 gulp.task('zip', () => {
-  return gulp.src('dist/*')
+  return gulp.src('./{content.js,manifest.json}')
     .pipe(zip('approval-counter.zip'))
     .pipe(gulp.dest('dist'))
 })
-
 
 gulp.task('clean', () => {
   return del([
@@ -24,4 +14,4 @@ gulp.task('clean', () => {
   ]);
 });
 
-gulp.task('default', gulp.series('clean', 'copy', 'zip'))
+gulp.task('default', gulp.series('clean', 'zip'))
